4cabc5c70d0090b453428c8680b348a42e9a33b7
Lucio-Gabriel <luciogabrieldeazevedoo@gmail.com>
2026-02-10 18:32:22 -0300

    feat: ensure tasks are listed on Tasks component

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -5,8 +5,17 @@ import Sun from "../assets/icons/sun.svg?react";
 import CloudSun from "../assets/icons/cloud-sun.svg?react";
 import Moon from "../assets/icons/moon.svg?react";
 import TaskSeparator from "./TaskSeparator";
+import { useState } from "react";
+import TASKS from "../constants/tasks";
+import TaskItem from "./TaskItem";
 
 const Tasks = () => {
+  const [tasks] = useState(TASKS);
+
+  const morningTasks = tasks.filter((task) => task.time === "morning");
+  const afternoonTasks = tasks.filter((task) => task.time === "afternoon");
+  const eveningTasks = tasks.filter((task) => task.time === "evening");
+
   return (
     <div className="w-full px-8 py-16">
       <div className="flex w-full justify-between">
@@ -33,14 +42,26 @@ const Tasks = () => {
       <div className="rounded-xl bg-white p-6">
         <div className="space-y-3">
           <TaskSeparator title="Manhã" icon={<Sun />} />
+
+          {morningTasks.map((task) => (
+            <TaskItem key={task.id} task={task} />
+          ))}
         </div>
 
         <div className="my-6 space-y-3">
           <TaskSeparator title="Tarde" icon={<CloudSun />} />
+
+          {afternoonTasks.map((task) => (
+            <TaskItem key={task.id} task={task} />
+          ))}
         </div>
 
         <div className="space-y-3">
           <TaskSeparator title="Noite" icon={<Moon />} />
+
+          {eveningTasks.map((task) => (
+            <TaskItem key={task.id} task={task} />
+          ))}
         </div>
       </div>
     </div>
---
+++ src/constants/tasks.js
@@ -0,0 +1,46 @@
+const TASKS = [
+  {
+    id: 1,
+    title: "Estudar FSC",
+    description: "Estudar para a prova de FSC",
+    time: "morning",
+    status: "done",
+  },
+  {
+    id: 2,
+    title: "Ler livro",
+    description: "Ler o livro do essencialismo",
+    time: "morning",
+    status: "done",
+  },
+  {
+    id: 3,
+    title: "Treinar",
+    description: "Academia",
+    time: "afternoon",
+    status: "in_progress",
+  },
+  {
+    id: 4,
+    title: "Fazer compras",
+    description: "Compra os ingredientes para o jantar",
+    time: "afternoon",
+    status: "in_progress",
+  },
+  {
+    id: 5,
+    title: "Descansar",
+    description: "Asssitir um filme",
+    time: "evening",
+    status: "not_started",
+  },
+  {
+    id: 6,
+    title: "Meditar",
+    description: "Meditar por 10 minutos",
+    time: "evening",
+    status: "not_started",
+  },
+];
+
+export default TASKS;
